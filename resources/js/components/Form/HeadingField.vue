d6bd682cf2df2a7dc42b30dc28e86fd77feb21a8
D.Thomas <dominic.thomas76@gmail.com>
2020-06-02 14:28:45 +0200

    fix #2 Add Heading Management

---
+++ resources/js/components/Form/HeadingField.vue
@@ -0,0 +1,45 @@
+<template>
+  <field-wrapper>
+    <div v-if="shouldDisplayAsHtml" v-html="field.value" :class="classes" />
+    <div v-else :class="classes">
+      <p>{{ field.value }}</p>
+    </div>
+  </field-wrapper>
+</template>
+<script>
+export default {
+  props: {
+    resourceName: {
+      type: String,
+      require: true,
+    },
+    field: {
+      type: Object,
+      require: true,
+    },
+  },
+
+  created() {
+    this.field.fill = () => {}
+  },
+
+  mounted() {
+    this.$el.classList.add('w-full')
+  },
+
+  computed: {
+    classes: () => [
+      'bg-20',
+      'remove-last-margin-bottom',
+      'leading-normal',
+      'w-full',
+      'py-4',
+      'px-8',
+    ],
+
+    shouldDisplayAsHtml() {
+      return this.field.asHtml || false
+    },
+  },
+}
+</script>
--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,7 +1,12 @@
 import DefaultField from './components/DefaultField'
 import PanelItem from './components/PanelItem'
+import DetailHeadingField from './components/Detail/HeadingField'
+import FormHeadingField from './components/Form/HeadingField'
+
 
 Nova.booting((Vue, router, store) => {
   Vue.component('default-field', DefaultField)
   Vue.component('panel-item', PanelItem)
+  Vue.component('detail-heading-field', DetailHeadingField)
+  Vue.component('form-heading-field', FormHeadingField)
 })
